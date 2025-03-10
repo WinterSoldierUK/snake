053a7af8dbfa331a94aebd7352bf0b8f2f8070c2
WinterSoldierUK <mattdx66@googlemail.com>
2025-03-10 15:56:56 +0000

    Added Score Code

--- Snake.py
+++ Snake.py
@@ -22,7 +22,7 @@ canvas.pack()
 window.update()
 
 window_width = canvas.winfo_width()
-window_height = canvas.winfo_height()  
+window_height = canvas.winfo_height()
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
@@ -35,8 +35,9 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 snake = Tile(5*TILE_SIZE, 5*TILE_SIZE)
 food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
 snake_body = []
-velocityX =0
-velocityY =0
+velocityX = 0
+velocityY = 0
+score = 0
 
 #Set direction of snake - note the and statements stop you from doubling back on yourself
 def changeDirection(event):
@@ -47,21 +48,22 @@ def changeDirection(event):
     elif (event.keysym == "Down" and velocityY != -1):
         velocityX = 0
         velocityY = 1
-    elif (event.keysym == "Left" and velocityX != 1): 
+    elif (event.keysym == "Left" and velocityX != 1):
         velocityX = -1
-        velocityY = 0  
+        velocityY = 0
     elif (event.keysym == "Right" and velocityX != -1):
         velocityX = 1
         velocityY = 0
 
 def move():
-    global snake
+    global snake, score
 
     #does the snake hit food?
     if (snake.x == food.x and snake.y == food.y):
         snake_body.append(Tile(food.x, food.y))
         food.x = random.randint(0, COLUMNS - 1) * TILE_SIZE
         food.y = random.randint(0, ROWS - 1) * TILE_SIZE
+        score += 1
 
     #set snakes new position based on velocity
     snake.x += velocityX * TILE_SIZE
@@ -71,17 +73,20 @@ def draw():
     global snake
     #update the snake position
     move()
-    
+
     #blank out the screen
     canvas.delete("all")
 
     #redraw the snake head and food
     canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill="red")
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill="lime green")
-    
+
     #redraw snake body
     for tile in snake_body:
         canvas.create_rectangle(tile.x, tile.y, tile.x + TILE_SIZE, tile.y + TILE_SIZE, fill="lime green")
+    
+    #redraw score
+    canvas.create_text(50, 10, text=f"Score: {score}", fill="white", font=("Arial", 16))
 
     #running at 10fps
     window.after(100, draw)
